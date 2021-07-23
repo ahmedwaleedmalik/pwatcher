7801034cb250e3991b4508115e720a5e0c35b4e1
Waleed Malik <ahmedwaleedmalik@gmail.com>
2021-07-23 18:58:42 +0500

    Add filter to only respond to pods that comply with pod and namespace level annotations

--- controllers/pod_controller.go
+++ controllers/pod_controller.go
@@ -69,7 +69,7 @@ func (r *PodReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 	// Object has been retrieved successfully at this point
 	// Add TimestampAnnotation to pod if doesn't already exist
 	if _, ok := instance.ObjectMeta.Annotations[TimestampAnnotation]; !ok {
-		// Annotation the pod with 
+		// Annotation the pod with Timestamp
 		return r.annotateResource(instance)
 	}
 
@@ -82,5 +82,6 @@ func (r *PodReconciler) SetupWithManager(mgr ctrl.Manager) error {
 		For(&corev1.Pod{}).
 		WithEventFilter(ignoreDeletePredicate()).
 		WithEventFilter(ignoreUpdatePredicate()).
+		WithEventFilter(filterCreatePredicate(mgr.GetClient())).
 		Complete(r)
 }
--- controllers/pod_controller_util.go
+++ controllers/pod_controller_util.go
@@ -1,8 +1,12 @@
 package controllers
 
 import (
+	"context"
 	"time"
 
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 )
@@ -35,4 +39,34 @@ func ignoreUpdatePredicate() predicate.Predicate {
 			return false
 		},
 	}
+}
+
+// filterCreatePredicate will apply filters based on pod and namespace annotations
+func filterCreatePredicate(client client.Client) predicate.Predicate {
+	return predicate.Funcs{
+		CreateFunc: func(e event.CreateEvent) bool {
+			pod := e.Object.(*corev1.Pod)
+			return isObservedPod(client, pod)
+		},
+	}
+}
+
+func isObservedPod(client client.Client, pod *corev1.Pod) bool {
+	// Consider resource for reconciliation only if it has the required annotation
+	if _, ok := pod.GetAnnotations()["timestamp"]; ok {
+		return true
+	}
+
+	// Retrieve namespace
+	namespace := &corev1.Namespace{}
+	err := client.Get(context.TODO(), types.NamespacedName{Name: pod.ObjectMeta.Namespace}, namespace)
+	if err != nil {
+		return false
+	}
+
+	// Consider resource for reconciliation only if the namespace in which it exists has the required annotation
+	if _, ok := namespace.GetAnnotations()["timestamp"]; ok {
+		return true
+	}
+	return false
 }
